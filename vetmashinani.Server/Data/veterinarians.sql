5570c8c1b430148d98fdaa45fc997381879ac8f2
Michael Kasuku <michaelotienokasuku@gmail.com>
2025-03-07 14:52:45 +0300

    images updated

--- vetmashinani.Server/Data/veterinarians.sql
+++ vetmashinani.Server/Data/veterinarians.sql
@@ -1,4 +1,6 @@
 INSERT INTO Veterinarians(Email) VALUES 
-('vet1@vetmashinani.com'),
-('vet2@vetmashinani.com'),
-('vet3@vetmashinani.com');
+('josphine@vetmashinani.com'),
+('michael@vetmashinani.com'),
+('daisy@vetmashinani.com');
+
+Edrawmap
